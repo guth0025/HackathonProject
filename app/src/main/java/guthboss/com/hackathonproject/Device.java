58da87c18c65a13752dbd884f6f88d7fa3846829
Shawn <sh.emami65@gmail.com>
2017-03-24 20:01:08 -0400

    added more checking for peer connect

---
+++ app/src/main/java/guthboss/com/hackathonproject/Device.java
@@ -0,0 +1,52 @@
+package guthboss.com.hackathonproject;
+
+import android.net.wifi.p2p.WifiP2pDevice;
+
+/**
+ * Created by Shariar on 3/24/2017.
+ */
+
+public class Device {
+
+    private String name, address;
+
+    public Device( WifiP2pDevice device){
+        name = device.deviceName;
+        address = device.deviceAddress;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Device device = (Device) o;
+
+        if (!name.equals(device.name)) return false;
+        return address.equals(device.address);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name.hashCode();
+        result = 31 * result + address.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Device{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+}
--- app/src/main/java/guthboss/com/hackathonproject/WiFiDirectBroadcastReceiver.java
+++ app/src/main/java/guthboss/com/hackathonproject/WiFiDirectBroadcastReceiver.java
@@ -11,17 +11,19 @@ import android.net.wifi.p2p.WifiP2pManager;
 import android.net.wifi.p2p.WifiP2pManager.Channel;
 import android.net.wifi.p2p.WifiP2pManager.PeerListListener;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.LinkedList;
 
 /**
  * A BroadcastReceiver that notifies of important Wi-Fi p2p events.
  */
 public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
 
+    private HashSet<Device> devices;
     private WifiP2pManager mManager;
     private Channel mChannel;
     private MainActivity mActivity;
-    private PeerListListener myPeerListListener;
     private IntentFilter mIntentFilter;
 
     public WiFiDirectBroadcastReceiver(WifiP2pManager manager, Channel channel, MainActivity activity) {
@@ -29,6 +31,8 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
         this.mManager = manager;
         this.mChannel = channel;
         this.mActivity = activity;
+
+        devices = new HashSet<>( 50);
 
         mIntentFilter = new IntentFilter();
         mIntentFilter.addAction(WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION);
@@ -78,35 +82,37 @@ public class WiFiDirectBroadcastReceiver extends BroadcastReceiver {
         String action = intent.getAction();
 
         if (WifiP2pManager.WIFI_P2P_STATE_CHANGED_ACTION.equals(action)) {
+            mActivity.addTextToChat( "STATE_CHANGED_ACTION");
             // Check to see if Wi-Fi is enabled and notify appropriate activity
             int state = intent.getIntExtra(WifiP2pManager.EXTRA_WIFI_STATE, -1);
             if (state == WifiP2pManager.WIFI_P2P_STATE_ENABLED) {
-                // Wifi P2P is enabled
+                mActivity.addTextToChat( "WIFI direct available");
             } else {
-                // Wi-Fi P2P is not enabled
+                mActivity.addTextToChat( "WIFI direct NOT available");
             }
         } else if (WifiP2pManager.WIFI_P2P_PEERS_CHANGED_ACTION.equals(action)) {
+            mActivity.addTextToChat( "PEERS_CHANGED_ACTION");
             // Call WifiP2pManager.requestPeers() to get a list of current peers
             if (mManager != null) {
-                myPeerListListener = new PeerListListener() {
+                PeerListListener myPeerListListener = new PeerListListener() {
                     @Override
                     public void onPeersAvailable(WifiP2pDeviceList wifiP2pDeviceList) {
-                        Collection<WifiP2pDevice> list = wifiP2pDeviceList.getDeviceList();
-                        StringBuffer strBuff;
-                        for( WifiP2pDevice d : list){
-                            strBuff = new StringBuffer();
-                            strBuff.append("Name: ").append(d.deviceName);
-                            strBuff.append(System.lineSeparator());
-                            strBuff.append("Address: ").append(d.deviceAddress);
-                            mActivity.addTextToChat( strBuff.toString());
+                        Device device;
+                        for( WifiP2pDevice d : wifiP2pDeviceList.getDeviceList()){
+                            device = new Device(d);
+                            if( devices.add( device)){
+                                mActivity.addTextToChat( device.toString());
+                            }
                         }
                     }
                 };
                 mManager.requestPeers(mChannel, myPeerListListener);
             }
         } else if (WifiP2pManager.WIFI_P2P_CONNECTION_CHANGED_ACTION.equals(action)) {
+            mActivity.addTextToChat( "CONNECTION_CHANGED_ACTION");
             // Respond to new connection or disconnections
         } else if (WifiP2pManager.WIFI_P2P_THIS_DEVICE_CHANGED_ACTION.equals(action)) {
+            mActivity.addTextToChat( "DEVICE_CHANGED_ACTION");
             // Respond to this device's wifi state changing
         }
     }
